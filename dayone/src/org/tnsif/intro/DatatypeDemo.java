6f7b8885f8059a2bd6dca35c1280b1b4b4f2c836
student <student@ioe-aids-188>
2023-02-02 11:53:43 +0530

    FIRST COMMIT

---
+++ dayone/src/org/tnsif/intro/DatatypeDemo.java
@@ -0,0 +1,26 @@
+package org.tnsif.intro;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//byte type
+		byte a=127;
+		
+		int b=128;
+		
+		float c=12/5;
+		float d=5/2.5f;
+		
+		double e=12.5;
+		double f=12d/5d;
+		
+		System.out.println("The value of a is:" +a);
+		System.out.println("The value of a is:" +b);
+		System.out.println("The value of a is:" +c);
+		System.out.println("The value of a is:" +d);
+		System.out.println("The value of a is:" +e);
+		System.out.println("The value of a is:" +f);
+	}
+
+}
---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,10 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
