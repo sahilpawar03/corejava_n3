0c14a225fffc40e50c5c07855d0dba69263cc34a
User <User@DESKTOP-BONKQR8>
2023-02-04 14:35:16 +0530

    Second Commit

---
+++ daytwo/src/org/tnsif/DecisionMaking/IfElseDemo.java
@@ -0,0 +1,27 @@
+package org.tnsif.DecisionMaking;
+
+import java.util.Scanner;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+
+		if(a==b)
+			System.out.println("a is equal to b");
+		else 
+			System.out.println("a is not equal to b");
+
+
+		//output
+		/*
+		 12
+		23
+		a is not equal to b
+		*/
+	}
+
+}
