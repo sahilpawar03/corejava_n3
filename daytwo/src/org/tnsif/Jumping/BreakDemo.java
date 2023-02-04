7967944fc741300d0d8cc0fb5ac2c29a5a56ed1c
User <User@DESKTOP-BONKQR8>
2023-02-04 14:37:53 +0530

    Third Commit

---
+++ daytwo/src/org/tnsif/Jumping/BreakDemo.java
@@ -0,0 +1,16 @@
+package org.tnsif.Jumping;
+
+import java.util.Scanner;
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+			if(i==4)
+				break;
+		}
+
+	}
+
+}
---
+++ daytwo/src/org/tnsif/Jumping/ContinueDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.Jumping;
+
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+	
+		for(int i=1;i<=10;i++)
+		{
+			if(i==4)
+				continue;
+			System.out.print(i+" ");
+		}
+	
+
+	}
+
+}
