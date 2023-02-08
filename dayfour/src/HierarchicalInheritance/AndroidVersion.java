7e0983d0c962726e31c70d8fb85143a40d72ad30
User <User@DESKTOP-BONKQR8>
2023-02-08 17:08:38 +0530

    Fourth Commit

--- dayfour/src/HierarchicalInheritance/AndroidVersion.java
+++ dayfour/src/HierarchicalInheritance/AndroidVersion.java
@@ -2,4 +2,32 @@ package HierarchicalInheritance;
 
 public class AndroidVersion {
 
+private String type;
+
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public AndroidVersion(String type) {
+		super();
+		this.type = type;
+	}
+	
+	
+	
+	public AndroidVersion() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "AndroidVersion [type=" + type + ", getType()=" + getType() + "]";
+	}
+	
 }
