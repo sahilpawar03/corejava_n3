806dc3bd6756c170d3e48ed033dc88b14d092a1d
User <User@DESKTOP-BONKQR8>
2023-02-13 10:53:20 +0530

    First Commit

---
+++ assignmenttwo/src/org/tnsif/stringdemo/StringSubClass.java
@@ -0,0 +1,14 @@
+package org.tnsif.stringdemo;
+//String is a final class and it is immutable
+//can't inherit any final class
+
+
+/*public class StringSubClass extends String{
+
+	public static void main(String[] args) {
+	
+	}
+
+}
+
+*/
